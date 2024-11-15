f9904348e51cf4a8c2493fa00d59d08f5a086411
Lee-412 <leeduchht@gmail.com>
2024-11-15 22:47:41 +0700

    fix(database): fix docker up with pgdata

--- database/initdb/postgres-init.sql
+++ database/initdb/postgres-init.sql
@@ -1,10 +1,18 @@
--- init.sql
-DO $$
+
+
+DO $$ 
 BEGIN
+    -- Kiểm tra xem role 'postgres' đã tồn tại chưa, nếu chưa thì tạo mới
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'postgres') THEN
         CREATE ROLE postgres WITH LOGIN PASSWORD '123456';
+    END IF;
+    
+    -- Kiểm tra xem database 'mydatabase' đã tồn tại chưa, nếu chưa thì tạo mới
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'postgres_url') THEN
+        CREATE DATABASE postgres_url;
     END IF;
 END
 $$;
 
-GRANT ALL PRIVILEGES ON DATABASE mydatabase TO postgres;
+-- Cấp quyền tất cả cho role 'postgres' trên database 'mydatabase'
+GRANT ALL PRIVILEGES ON DATABASE postgres_url TO postgres;
